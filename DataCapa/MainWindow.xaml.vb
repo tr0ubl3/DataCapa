7b1890a5a6260770b87b9d52953abff7b84ce191
tr0ubl3 <mentor.1985@gmail.com>
2017-04-11 23:55:42 +0300

    added menu skeleton and icon files

---
+++ DataCapa/MainWindow.xaml.vb
@@ -0,0 +1,12 @@
+﻿Imports System
+Imports FontAwesome.WPF
+Class MainWindow
+
+    Private Sub test_Click(sender As Object, e As RoutedEventArgs) Handles test.Click
+        MsgBox(Environment.UserDomainName)
+    End Sub
+
+    Private Sub mnuExit_Click(sender As Object, e As RoutedEventArgs)
+        Close()
+    End Sub
+End Class
