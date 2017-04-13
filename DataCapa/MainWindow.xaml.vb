163b72ebac93909bb91980359877a2d23058c380
tr0ubl3 <mentor.1985@gmail.com>
2017-04-13 23:05:22 +0300

    removed cache

--- DataCapa/MainWindow.xaml.vb
+++ DataCapa/MainWindow.xaml.vb
@@ -1,12 +1,11 @@
 ﻿Imports System
 Imports FontAwesome.WPF
 Class MainWindow
-
-    Private Sub test_Click(sender As Object, e As RoutedEventArgs) Handles test.Click
-        MsgBox(Environment.UserDomainName)
-    End Sub
-
     Private Sub mnuExit_Click(sender As Object, e As RoutedEventArgs)
         Close()
+    End Sub
+
+    Private Sub mnuLogin_Click(sender As Object, e As RoutedEventArgs)
+        yeld.NavigationService.Navigate(New Page1)
     End Sub
 End Class
---
+++ DataCapa/Page1.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class Page1
+    Private Sub test_Click(sender As Object, e As RoutedEventArgs) Handles test.Click
+        MsgBox(Environment.UserDomainName)
+    End Sub
+
+    Private Sub test2_Click(sender As Object, e As RoutedEventArgs) Handles test2.Click
+        MsgBox(Environment.TickCount)
+    End Sub
+End Class
